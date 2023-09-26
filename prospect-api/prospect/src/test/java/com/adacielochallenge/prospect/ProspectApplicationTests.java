a03be50dd3b10f6ce096182f9166bf357837c109
Vitor Loura <vitorloura@gmail.com>
2023-09-25 22:25:06 -0300

    Adicionando arquivos iniciais da API

---
+++ prospect-api/prospect/src/test/java/com/adacielochallenge/prospect/ProspectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adacielochallenge.prospect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProspectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
