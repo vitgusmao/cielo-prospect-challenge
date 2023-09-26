584e197b7627763f3a4b1a25a37bbb592552032b
Vitor Loura <vitorloura@gmail.com>
2023-09-26 22:15:35 +0000

    Inicializando repositório de pessoa física

---
+++ prospect/src/main/java/com/adacielochallenge/prospect/repository/NaturalPersonRepository.java
@@ -0,0 +1,9 @@
+package com.adacielochallenge.prospect.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.adacielochallenge.prospect.model.NaturalPerson;
+
+public interface NaturalPersonRepository extends CrudRepository<NaturalPerson, Long> {
+
+}
