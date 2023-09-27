f5574fb96c8c5dffd294ab82b5a96e741585fec1
Vitor Loura <vitorloura@gmail.com>
2023-09-27 02:40:21 +0000

    Adicionando queries para itens únicos

--- prospect/src/main/java/com/adacielochallenge/prospect/repository/LegalEntityRepository.java
+++ prospect/src/main/java/com/adacielochallenge/prospect/repository/LegalEntityRepository.java
@@ -1,9 +1,11 @@
 package com.adacielochallenge.prospect.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.adacielochallenge.prospect.model.LegalEntity;
 
 public interface LegalEntityRepository extends CrudRepository<LegalEntity, Long> {
-
+    List<LegalEntity> findByCnpj(String cnpj);
 }
--- prospect/src/main/java/com/adacielochallenge/prospect/repository/NaturalPersonRepository.java
+++ prospect/src/main/java/com/adacielochallenge/prospect/repository/NaturalPersonRepository.java
@@ -1,9 +1,11 @@
 package com.adacielochallenge.prospect.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.adacielochallenge.prospect.model.NaturalPerson;
 
 public interface NaturalPersonRepository extends CrudRepository<NaturalPerson, Long> {
-
+    List<NaturalPerson> findByCpf(String cpf);
 }
