7abd9fe3c3eed0db0b205215f11fda053228bfba
Vitor Loura <vitorloura@gmail.com>
2023-09-27 04:08:28 +0000

    Transformando em cascade

--- prospect/src/main/java/com/adacielochallenge/prospect/model/Client.java
+++ prospect/src/main/java/com/adacielochallenge/prospect/model/Client.java
@@ -1,5 +1,8 @@
 package com.adacielochallenge.prospect.model;
 
+import java.util.Date;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.DiscriminatorType;
 import jakarta.persistence.Entity;
@@ -9,6 +12,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -18,17 +23,24 @@ import lombok.ToString;
 @ToString
 @Getter
 @DiscriminatorColumn(name = "client_type", discriminatorType = DiscriminatorType.STRING)
+// TODO: Testar para melhor escalabilidade
+// discriminatorType = DiscriminatorType.INTEGER,
+// name = "client_type_id",
+// columnDefinition = "TINYINT(1)"
 public class Client {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne(mappedBy = "client")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdOn = new Date();
+
+    @OneToOne(mappedBy = "client", cascade = CascadeType.ALL)
     @Setter
     private NaturalPerson naturalPerson;
 
-    @OneToOne(mappedBy = "client")
+    @OneToOne(mappedBy = "client", cascade = CascadeType.ALL)
     @Setter
     private LegalEntity legalEntity;
 }
