0c716549e6e5e4f6df28bae40aa41ae6e77e358b
Vitor Loura <vitorloura@gmail.com>
2023-09-29 02:58:30 +0000

    Melhorando documentação da api

--- prospect/src/main/java/com/adacielochallenge/prospect/controller/ClientController.java
+++ prospect/src/main/java/com/adacielochallenge/prospect/controller/ClientController.java
@@ -23,8 +23,9 @@ import com.adacielochallenge.prospect.dto.ClientUpdateDTO;
 import com.adacielochallenge.prospect.dto.LegalEntityCreateDTO;
 import com.adacielochallenge.prospect.dto.NaturalPersonCreateDTO;
 import com.adacielochallenge.prospect.model.Client;
+import com.adacielochallenge.prospect.model.LegalEntity;
+import com.adacielochallenge.prospect.model.NaturalPerson;
 import com.adacielochallenge.prospect.service.ClientService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
@@ -54,140 +55,142 @@ public class ClientController {
     })
     @PostMapping("/legal-entities")
     @Validated
-    public ResponseEntity<String> createLegalEntity(@Valid @RequestBody LegalEntityCreateDTO legalEntityCreateDTO) {
+    public ResponseEntity<Object> createLegalEntity(@Valid @RequestBody LegalEntityCreateDTO legalEntityCreateDTO) {
         LOG.debug("create request arrived: %s".formatted(legalEntityCreateDTO.toString()));
         try {
-            clientService.createLegalEntity(legalEntityCreateDTO);
-            return new ResponseEntity<>(this.generateResponseMessageJson("successfully created a new client prospect."),
-                    HttpStatus.CREATED);
+            LegalEntity client = clientService.createLegalEntity(legalEntityCreateDTO);
+            return new ResponseEntity<>(client, HttpStatus.CREATED);
         } catch (DataIntegrityViolationException e) {
-            return ResponseEntity.badRequest()
-                    .body(this.generateResponseMessageJson("client already exists."));
+            return ResponseEntity
+                    .badRequest()
+                    .body(this.generateErrorMessage("cliente já existe."));
         } catch (Exception e) {
             LOG.error(e.getMessage());
-            return ResponseEntity.internalServerError()
-                    .body(this.generateResponseMessageJson("error creating a new client prospect."));
+            return ResponseEntity
+                    .internalServerError()
+                    .body(this.generateErrorMessage("erro na criação do prospect do cliente."));
         }
     }
 
     @Operation(summary = "create a new natural person prospect")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "successfully created a new natural person prospect")
+            @ApiResponse(responseCode = "201", description = "successfully created a new natural person prospect"),
+            @ApiResponse(responseCode = "400", description = "client already exists or data provided is not valid."),
+            @ApiResponse(responseCode = "500", description = "unexpected error creating a new client prospect.")
     })
     @PostMapping("/natural-persons")
     @Validated
-    public ResponseEntity<String> createNaturalPerson(
+    public ResponseEntity<Object> createNaturalPerson(
             @Valid @RequestBody NaturalPersonCreateDTO naturalPersonCreateDTO) {
 
         LOG.info("create request arrived: %s".formatted(naturalPersonCreateDTO.toString()));
         try {
-            clientService.createNaturalPerson(naturalPersonCreateDTO);
-            return new ResponseEntity<>(this.generateResponseMessageJson("successfully created a new client prospect."),
-                    HttpStatus.CREATED);
+            NaturalPerson client = clientService.createNaturalPerson(naturalPersonCreateDTO);
+            return new ResponseEntity<>(client, HttpStatus.CREATED);
         } catch (DataIntegrityViolationException e) {
             return ResponseEntity.badRequest()
-                    .body(this.generateResponseMessageJson("client prospect already exists."));
+                    .body(this.generateErrorMessage("cliente já existe."));
         } catch (Exception e) {
             LOG.error(e.getMessage());
             return ResponseEntity.internalServerError()
-                    .body(this.generateResponseMessageJson("error creating a new client prospect."));
+                    .body(this.generateErrorMessage("erro na criação do prospect do cliente."));
         }
     }
 
     @Operation(summary = "get the list off all the clients")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "successfully retrieved all clients")
+            @ApiResponse(responseCode = "200", description = "successfully retrieved all clients"),
+            @ApiResponse(responseCode = "500", description = "unexpected error retrieving clients list.")
     })
     @GetMapping("/clients")
-    public ResponseEntity<String> list() {
+    public ResponseEntity<Object> list() {
 
         try {
             List<Client> clientList = clientService.listClients();
-            String jsonString = objectMapper.writeValueAsString(clientList);
-
-            return ResponseEntity.ok(jsonString);
-        } catch (JsonProcessingException e) {
+            return ResponseEntity.ok(clientList);
+        } catch (Exception e) {
             LOG.error(e.getMessage());
-            return ResponseEntity.internalServerError()
-                    .body(this.generateResponseMessageJson("erro ao ler lista de clientes"));
+            return ResponseEntity
+                    .internalServerError()
+                    .body(this.generateErrorMessage("erro ao ler lista de clientes"));
         }
 
     }
 
     @Operation(summary = "retrieve one detailed client")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "successfully retrieved client by path variable {id}")
+            @ApiResponse(responseCode = "200", description = "successfully retrieved client by path variable {id}."),
+            @ApiResponse(responseCode = "404", description = "client not found."),
+            @ApiResponse(responseCode = "500", description = "unexpected error.")
     })
     @GetMapping("/clients/{id}")
-    public ResponseEntity<String> retrieve(@PathVariable Long id) {
+    public ResponseEntity<Object> retrieve(@PathVariable Long id) {
         try {
             Client client = clientService.retrieveClient(id);
-            return ResponseEntity.ok().body(objectMapper.writeValueAsString(client));
-        } catch (JsonProcessingException e) {
-            LOG.error(e.getMessage());
-            return ResponseEntity.internalServerError()
-                    .body(this.generateResponseMessageJson("erro processar retorno do cliente"));
+            return ResponseEntity.ok().body(client);
         } catch (EmptyResultDataAccessException e) {
             return ResponseEntity.notFound().build();
         } catch (Exception e) {
             LOG.error(e.getMessage());
-            return ResponseEntity.internalServerError()
-                    .body(this.generateResponseMessageJson("Ocorreu um erro inesperado."));
+            return ResponseEntity
+                    .internalServerError()
+                    .body(this.generateErrorMessage("ocorreu um erro inesperado."));
         }
     }
 
     @Operation(summary = "update one client")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "successfully updated prospect.")
+            @ApiResponse(responseCode = "200", description = "successfully updated prospect."),
+            @ApiResponse(responseCode = "400", description = "malformed request body."),
+            @ApiResponse(responseCode = "404", description = "client not found."),
+            @ApiResponse(responseCode = "500", description = "unexpected error.")
     })
     @PutMapping("/clients/{id}")
     @Validated
-    public ResponseEntity<String> update(@PathVariable Long id, @Valid @RequestBody ClientUpdateDTO clientUpdateDTO) {
+    public ResponseEntity<Object> update(@PathVariable Long id, @Valid @RequestBody ClientUpdateDTO clientUpdateDTO) {
         try {
             Client client = clientService.updateClient(id, clientUpdateDTO);
-            if (client != null) {
-                return ResponseEntity.badRequest()
-                        .body(this.generateResponseMessageJson("corpo da requisição inválido"));
+            if (client == null) {
+                return ResponseEntity
+                        .ok()
+                        .body(this.generateErrorMessage("prospect atualizado com sucesso"));
             }
-            return ResponseEntity.ok().body(this.generateResponseMessageJson("prospect atualizado com sucesso"));
+            return ResponseEntity
+                    .badRequest()
+                    .body(this.generateErrorMessage("corpo da requisição inválido"));
         } catch (EmptyResultDataAccessException e) {
             return ResponseEntity.notFound().build();
         } catch (Exception e) {
             LOG.error(e.getMessage());
-            return ResponseEntity.internalServerError()
-                    .body(this.generateResponseMessageJson("ocorreu um erro inesperado."));
+            return ResponseEntity
+                    .internalServerError()
+                    .body(this.generateErrorMessage("ocorreu um erro inesperado."));
         }
     }
 
     @Operation(summary = "delete one client")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "successfully deleted prospect.")
+            @ApiResponse(responseCode = "200", description = "successfully deleted prospect."),
+            @ApiResponse(responseCode = "500", description = "unexpected error.")
     })
     @DeleteMapping("/clients/{id}")
-    public ResponseEntity<String> delete(@PathVariable Long id) {
+    public ResponseEntity<Object> delete(@PathVariable Long id) {
         try {
             clientService.deleteClient(id);
-            return ResponseEntity.ok(this.generateResponseMessageJson("successfully deleted prospect."));
+            return ResponseEntity.ok(this.generateErrorMessage("successfully deleted prospect."));
 
         } catch (Exception e) {
             LOG.error(e.getMessage());
-            return ResponseEntity.internalServerError()
-                    .body(this.generateResponseMessageJson("ocorreu um erro inesperado."));
+            return ResponseEntity
+                    .internalServerError()
+                    .body(this.generateErrorMessage("ocorreu um erro inesperado."));
         }
     }
 
-    private String generateResponseMessageJson(String message) {
+    private Object generateErrorMessage(String message) {
         ObjectNode jsonNode = objectMapper.createObjectNode();
         jsonNode.put("message", message);
-
-        try {
-            String jsonString = objectMapper.writeValueAsString(jsonNode);
-            return jsonString;
-        } catch (JsonProcessingException e) {
-            String errorMessage = e.getMessage();
-            LOG.error(errorMessage);
-            return errorMessage;
-        }
+        return jsonNode;
     }
 
 }
